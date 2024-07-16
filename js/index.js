9716d5c074ff767a2e7b2999802df3107403bb57
Pizzaman333 <andrijmalchevv369@gmail.com>
2024-07-17 01:07:51 +0300

    All

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const age = 16;
+console.log(age);
+
+const name = "Andrew";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Успіх — це вміння рухатися від невдачі до невдачі, не втрачаючи ентузіазму. Вінстон Черчилль";
+console.log(myString);
+
+let myNumber = 3;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+let myName = prompt("Please enter your username");
+console.log("Thanks for using your username", myName);
+
+let myChoice = confirm("Please confirm your choice");
